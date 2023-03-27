a37a4e41c96a9a2372fb0f427712bd5546913631
samantariya <samantaislam355@gmail.com>
2023-03-27 12:03:25 +0600

    second commit

---
+++ my-react-app/src/components/Shop/Shop.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import "./Shop.css";
+
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  return (
+    <div className="shop-container">
+      <div className="product-container">
+        <h2>Products Coming Here:{products.length}</h2>
+      </div>
+      <div className="cart-container">
+        <h2>Order Summery</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
